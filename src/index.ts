fb093cd972a4d2e083d1683a6110da332e9e5614
Kristoffer Högberg <kristoffer.hogberg@apendo.se>
2023-09-25 11:10:02 +0200

    init commit

---
+++ src/index.ts
@@ -0,0 +1,71 @@
+import {ZBClient} from 'zeebe-node'
+import {getInput, setFailed} from "@actions/core";
+import fs from 'fs';
+
+const ZEEBE_CLIENT_ID = getInput('client_id')
+const ZEEBE_CLIENT_SECRET = getInput('client_secret')
+const CAMUNDA_CLUSTER_ID = getInput('cluster_id')
+const SOURCE = getInput('source');
+
+const zbc = new ZBClient({
+    camundaCloud: {
+        clientId: ZEEBE_CLIENT_ID,
+        clientSecret: ZEEBE_CLIENT_SECRET,
+        clusterId: CAMUNDA_CLUSTER_ID,
+        clusterRegion: "bru-2",
+    },
+});
+
+
+const getFilenamesInFolder = async (folderPath: string): Promise<string[]> => {
+    try {
+
+        return await fs.promises.readdir(folderPath);
+    } catch (error) {
+        console.error('Error reading folder:', error);
+        return [];
+    }
+};
+
+const deployBpmnModel = async () => {
+    try {
+
+        const filenames = await getFilenamesInFolder(SOURCE);
+
+        for (const file of filenames) {
+            console.log("File: " + file)
+            const res = await zbc.deployProcess(SOURCE + '/' + file)
+            console.log(res)
+
+        }
+
+    } catch (error) {
+
+        setFailed(error instanceof Error ? error.message : 'An error occurred');
+    }
+}
+
+
+const runWorkflow = async () => {
+
+    try {
+
+        await deployBpmnModel();
+
+    } catch (error) {
+        setFailed(error instanceof Error ? error.message : 'An error occurred');
+    }
+
+}
+
+runWorkflow()
+    .then(() => {
+        console.log("Workflow completed successfully.");
+        zbc.close().then(r => {
+        });
+    })
+    .catch((error) => {
+        console.error("Workflow failed:", error)
+        zbc.close().then(r => {
+        });
+    });
